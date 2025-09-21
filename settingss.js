7ebc9852fa7b8a1eb07b3d467ec5c7cc3d4f8f00
dennismwangi001 <136080030+dennismwangi001@users.noreply.github.com>
2025-09-21 10:15:56 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~d6MFQZKC#Neenm6D5uU8wOgzlMN8wdKnkBLb85GugEFTIw9wpIqc",  // add your Session Id here
+  OWNER_NUMBER: "254700005677", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
-  TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Kenya" //put your country timeZone....leave blank if u don't know.
 };
